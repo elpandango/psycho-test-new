eb885c3604005c70dd43324355b04c11acdcc1e9
Eugen Radchenko <elpandango@gmail.com>
2021-03-12 00:49:24 +0200

    Added third test answers

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 // Create express instance
 const app = express()
 const session = require('express-session');
+const cors = require('cors');
 
 // Require API routes
 const users = require('./routes/users')
@@ -14,6 +15,13 @@ app.use(bodyParser.json());
 app.use(
   session({secret: 'randomString', resave: false, saveUninitialized: false, cookie: {maxAge: 3600000}})
 );
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 // Import API Routes
 app.use(users)
